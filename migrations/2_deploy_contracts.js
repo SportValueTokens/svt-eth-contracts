e794b060ca64e784f34490ac6fee4f5b8a018ef6
Chris Popov <chrpopov@gmail.com>
2019-10-14 13:08:01 +0300

    test TokenSwap setup

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -55,6 +55,7 @@ module.exports = async function (deployer, network, accounts) {
     // const tokenSwapFactoryCricket = await deployer.deploy(SVCTokenSwapFactory, SportValueCoin.address, 3, 'cricket')
     // await deployer.deploy(Payout, 3, 'cricket', SportValueCoin.address, cricketPayoutsAccount.address)
 
+    web3Client.init(SportValueCoin.address)
     await web3Client.deployAssets(conf.tokens.football, tokenSwapFactoryFootball.address, tokenFactoryFootball.address, accounts[0])
     // web3Client.deployAssets(conf.tokens.basketball, tokenSwapFactoryBasketball.address, tokenFactoryBasketball.address, accounts[0])
     // web3Client.deployAssets(conf.tokens.cricket, tokenSwapFactoryCricket.address, tokenFactoryCricket.address, accounts[0])
--- test/contracts/tokenswap/SVCTokenSwapTest.js
+++ test/contracts/tokenswap/SVCTokenSwapTest.js
@@ -11,7 +11,7 @@ const expectRevert = require('../helpers').expectRevert
 contract('SVCTokenSwap', function (accounts) {
   let coinContract
   let playerTokenContract
-  let exchangeContract
+  let tokenSwapContract
   const creatorAccount = accounts[0]
   const user1Account = accounts[1]
   const user2Account = accounts[2]
@@ -30,14 +30,14 @@ contract('SVCTokenSwap', function (accounts) {
     await coinContract.transfer(user1Account, tenCoins, {from: creatorAccount})
     await coinContract.transfer(user2Account, tenCoins, {from: creatorAccount})
 
-    exchangeContract = await SVCTokenSwap.new(coinContract.address, playerTokenContract.address, {from: creatorAccount})
+    tokenSwapContract = await SVCTokenSwap.new(coinContract.address, playerTokenContract.address, {from: creatorAccount})
 
     // transfer assets to user2
     playerTokenContract.transfer(user2Account, twoCoins)
 
     // transfer SVC and asset to exchange
-    await coinContract.transfer(exchangeContract.address, tenCoins, {from: creatorAccount})
-    await playerTokenContract.transfer(exchangeContract.address, tenCoins, {from: creatorAccount})
+    await coinContract.transfer(tokenSwapContract.address, tenCoins, {from: creatorAccount})
+    await playerTokenContract.transfer(tokenSwapContract.address, tenCoins, {from: creatorAccount})
   }
 
   describe('Asset purchase', () => {
@@ -45,11 +45,11 @@ contract('SVCTokenSwap', function (accounts) {
 
     it('a user should be able to buy an asset with SVC', async () => {
       // first user1Account should approve the contract to spend its SVC
-      let res = await coinContract.approve(exchangeContract.address, twoCoins, {from: user1Account})
+      let res = await coinContract.approve(tokenSwapContract.address, twoCoins, {from: user1Account})
       assert.ok(res)
 
       // then buy 1 player token
-      await exchangeContract.buy(oneCoin, {from: user1Account})
+      await tokenSwapContract.buy(oneCoin, {from: user1Account})
 
       // we expect 1 token bought 1.00 SVC to be debited from user1 acc
       let coinBalance = await coinContract.balanceOf.call(user1Account)
@@ -60,27 +60,27 @@ contract('SVCTokenSwap', function (accounts) {
       expect(assetBalance).to.eq.BN(oneCoin)
 
       // price should have increased
-      let price = await exchangeContract.getAssetPrice.call()
+      let price = await tokenSwapContract.getAssetPrice.call()
       expect(price).to.eq.BN(12222)
     })
 
     it('should fail if buyer has no money', async () => {
       const user3Account = accounts[3]
       // user3 approves the contract to spend SVC
-      let res = await coinContract.approve(exchangeContract.address, oneCoin, {from: user3Account})
+      let res = await coinContract.approve(tokenSwapContract.address, oneCoin, {from: user3Account})
       assert.ok(res)
-      await expectRevert(exchangeContract.buy(oneCoin, {from: user3Account}))
+      await expectRevert(tokenSwapContract.buy(oneCoin, {from: user3Account}))
     })
 
     it('should fail if contract has no stock', async () => {
       // user1 approve the contract to spend SVC
-      let res = await coinContract.approve(exchangeContract.address, hundredCoins, {from: user1Account})
+      let res = await coinContract.approve(tokenSwapContract.address, hundredCoins, {from: user1Account})
       assert.ok(res)
-      await expectRevert(exchangeContract.buy(hundredCoins, {from: user1Account}))
+      await expectRevert(tokenSwapContract.buy(hundredCoins, {from: user1Account}))
     })
 
     it('should fail if buyer has not allowed transfers', async () => {
-      await expectRevert(exchangeContract.buy(oneCoin, {from: user1Account}))
+      await expectRevert(tokenSwapContract.buy(oneCoin, {from: user1Account}))
     })
 
     // should not be able to have 0 assets
@@ -91,11 +91,11 @@ contract('SVCTokenSwap', function (accounts) {
 
     it('a user should be able to sell an asset for SVC', async () => {
       // user2 needs to allow the liquidity contract to spend his tokens
-      let res = await playerTokenContract.approve(exchangeContract.address, twoCoins, {from: user2Account})
+      let res = await playerTokenContract.approve(tokenSwapContract.address, twoCoins, {from: user2Account})
       assert.ok(res)
 
       // then sell 1 token
-      await exchangeContract.sell(oneCoin, {from: user2Account})
+      await tokenSwapContract.sell(oneCoin, {from: user2Account})
 
       // we expect 1 token to be sold for 1 SVC
       let coinBalance = await coinContract.balanceOf.call(user2Account)
@@ -106,27 +106,27 @@ contract('SVCTokenSwap', function (accounts) {
       expect(assetBalance).to.eq.BN(oneCoin)
 
       // price should have decreased
-      let price = await exchangeContract.getAssetPrice.call()
+      let price = await tokenSwapContract.getAssetPrice.call()
       expect(price).to.eq.BN(8181)
     })
 
     it('should fail if seller has no tokens', async () => {
       const user3Account = accounts[3]
       // user3 approves the contract to spend player tokens
-      let res = await playerTokenContract.approve(exchangeContract.address, oneCoin, {from: user3Account})
+      let res = await playerTokenContract.approve(tokenSwapContract.address, oneCoin, {from: user3Account})
       assert.ok(res)
-      await expectRevert(exchangeContract.sell(oneCoin, {from: user3Account}))
+      await expectRevert(tokenSwapContract.sell(oneCoin, {from: user3Account}))
     })
 
     it('should fail if contract has no stock', async () => {
       // user1 approve the contract to spend SVC
-      let res = await playerTokenContract.approve(exchangeContract.address, oneCoin, {from: user1Account})
+      let res = await playerTokenContract.approve(tokenSwapContract.address, oneCoin, {from: user1Account})
       assert.ok(res)
-      await expectRevert(exchangeContract.sell(hundredCoins, {from: user1Account}))
+      await expectRevert(tokenSwapContract.sell(hundredCoins, {from: user1Account}))
     })
 
     it('should fail if seller has not allowed transfers', async () => {
-      await expectRevert(exchangeContract.sell(oneCoin, {from: user2Account}))
+      await expectRevert(tokenSwapContract.sell(oneCoin, {from: user2Account}))
     })
 
     // should not be able to have 0 assets
@@ -137,51 +137,51 @@ contract('SVCTokenSwap', function (accounts) {
 
     it('should add liquidity to the exchange', async () => {
       // approve transfers
-      let res = await playerTokenContract.approve(exchangeContract.address, tenCoins, {from: creatorAccount})
+      let res = await playerTokenContract.approve(tokenSwapContract.address, tenCoins, {from: creatorAccount})
       assert.ok(res)
-      res = await coinContract.approve(exchangeContract.address, tenCoins, {from: creatorAccount})
+      res = await coinContract.approve(tokenSwapContract.address, tenCoins, {from: creatorAccount})
       assert.ok(res)
 
-      await exchangeContract.addLiquidity(oneCoin, {from: creatorAccount})
+      await tokenSwapContract.addLiquidity(oneCoin, {from: creatorAccount})
 
       // we expect 1 coin and 1 token to be transferred
-      let coinBalance = await coinContract.balanceOf.call(exchangeContract.address)
+      let coinBalance = await coinContract.balanceOf.call(tokenSwapContract.address)
       console.log('Exchange SVC balance: ', coinBalance.toString())
       expect(coinBalance).to.eq.BN(new BN(11).mul(new BN(10).pow(new BN(DECIMALS))))
-      let assetBalance = await playerTokenContract.balanceOf.call(exchangeContract.address)
+      let assetBalance = await playerTokenContract.balanceOf.call(tokenSwapContract.address)
       console.log('Exchange Asset balance: ', assetBalance.toString())
       expect(assetBalance).to.eq.BN(new BN(11).mul(new BN(10).pow(new BN(DECIMALS))))
 
       // price should not have changed
-      let price = await exchangeContract.getAssetPrice.call()
+      let price = await tokenSwapContract.getAssetPrice.call()
       expect(price).to.eq.BN(10000)
     })
 
     it('should fail if not enough assets', async () => {
       // approve transfers
-      let res = await playerTokenContract.approve(exchangeContract.address, thousandCoins.mul(new BN(2)), {from: creatorAccount})
+      let res = await playerTokenContract.approve(tokenSwapContract.address, thousandCoins.mul(new BN(2)), {from: creatorAccount})
       assert.ok(res)
-      res = await coinContract.approve(exchangeContract.address, thousandCoins.mul(new BN(2)), {from: creatorAccount})
+      res = await coinContract.approve(tokenSwapContract.address, thousandCoins.mul(new BN(2)), {from: creatorAccount})
       assert.ok(res)
 
       // try to call addLiquidity
-      await expectRevert(exchangeContract.addLiquidity(thousandCoins.mul(new BN(2)), {from: creatorAccount}))
+      await expectRevert(tokenSwapContract.addLiquidity(thousandCoins.mul(new BN(2)), {from: creatorAccount}))
 
       // price should not have changed
-      let price = await exchangeContract.getAssetPrice.call()
+      let price = await tokenSwapContract.getAssetPrice.call()
       expect(price).to.eq.BN(10000)
     })
 
     it('should fail if asset not approved', async () => {
       // approve transfers
-      res = await coinContract.approve(exchangeContract.address, thousandCoins.mul(new BN(2)), {from: creatorAccount})
+      res = await coinContract.approve(tokenSwapContract.address, thousandCoins.mul(new BN(2)), {from: creatorAccount})
       assert.ok(res)
 
       // try to call addLiquidity
-      await expectRevert(exchangeContract.addLiquidity(oneCoin, {from: creatorAccount}))
+      await expectRevert(tokenSwapContract.addLiquidity(oneCoin, {from: creatorAccount}))
 
       // price should not have changed
-      let price = await exchangeContract.getAssetPrice.call()
+      let price = await tokenSwapContract.getAssetPrice.call()
       expect(price).to.eq.BN(10000)
     })
 
@@ -191,38 +191,38 @@ contract('SVCTokenSwap', function (accounts) {
     beforeEach(init)
 
     it('should remove liquidity from the exchange', async () => {
-      await exchangeContract.removeLiquidity(twoCoins, {from: creatorAccount})
+      await tokenSwapContract.removeLiquidity(twoCoins, {from: creatorAccount})
 
       // we expect 1 coin and 1 token to be transferred
-      let coinBalance = await coinContract.balanceOf.call(exchangeContract.address)
+      let coinBalance = await coinContract.balanceOf.call(tokenSwapContract.address)
       console.log('Exchange SVC balance: ', coinBalance.toString())
       expect(coinBalance).to.eq.BN(new BN(8).mul(new BN(10).pow(new BN(DECIMALS))))
-      let assetBalance = await playerTokenContract.balanceOf.call(exchangeContract.address)
+      let assetBalance = await playerTokenContract.balanceOf.call(tokenSwapContract.address)
       console.log('Exchange Asset balance: ', assetBalance.toString())
       expect(assetBalance).to.eq.BN(new BN(8).mul(new BN(10).pow(new BN(DECIMALS))))
 
       // price should not have changed
-      let price = await exchangeContract.getAssetPrice.call()
+      let price = await tokenSwapContract.getAssetPrice.call()
       expect(price).to.eq.BN(10000)
     })
 
     it('should remove all coins from the exchange', async () => {
-      await exchangeContract.removeLiquidity(tenCoins, {from: creatorAccount})
+      await tokenSwapContract.removeLiquidity(tenCoins, {from: creatorAccount})
 
       // we expect 1 coin and 1 token to be transferred
-      let coinBalance = await coinContract.balanceOf.call(exchangeContract.address)
+      let coinBalance = await coinContract.balanceOf.call(tokenSwapContract.address)
       console.log('Exchange SVC balance: ', coinBalance.toString())
       expect(coinBalance).to.eq.BN(0)
-      let assetBalance = await playerTokenContract.balanceOf.call(exchangeContract.address)
+      let assetBalance = await playerTokenContract.balanceOf.call(tokenSwapContract.address)
       console.log('Exchange Asset balance: ', assetBalance.toString())
       expect(assetBalance).to.eq.BN(0)
     })
 
     it('should fail if not enough assets', async () => {
-      await expectRevert(exchangeContract.removeLiquidity(thousandCoins.mul(new BN(2)), {from: creatorAccount}))
+      await expectRevert(tokenSwapContract.removeLiquidity(thousandCoins.mul(new BN(2)), {from: creatorAccount}))
 
       // price should not have changed
-      let price = await exchangeContract.getAssetPrice.call()
+      let price = await tokenSwapContract.getAssetPrice.call()
       expect(price).to.eq.BN(10000)
     })
 
